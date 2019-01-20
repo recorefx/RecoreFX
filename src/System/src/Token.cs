3de2d13a7ad340512284526fc1d704024bf31a3d
Brian Cristante <brian.cristante@gmail.com>
2019-01-20 22:33:24 +0000

    Merged PR 22: Remove Required from Token
    Remove Required from Token
    
    Related work items: #41

--- src/System/src/Token.cs
+++ src/System/src/Token.cs
@@ -3,20 +3,22 @@ namespace System
     // A non-null, non-empty string value where whitespace is not allowed.
     public sealed class Token
     {
-        public Required<string> Value { get; }
+        // This could be Required, but Token already makes that guarantee,
+        // along with it being nonzero length and no whitespace.
+        public string Value { get; }
 
-        public Token(Required<string> value)
+        public Token(string value)
         {
             if (value == string.Empty)
             {
                 throw new ArgumentException(Strings.TokenEmpty);
             }
 
-            foreach (char c in value.Value)
+            foreach (char c in value)
             {
                 if (char.IsWhiteSpace(c))
                 {
-                    throw new ArgumentException(string.Format(Strings.TokenWhitespace, value.Value));
+                    throw new ArgumentException(string.Format(Strings.TokenWhitespace, value));
                 }
             }
 
