ebbdd5753c352c6cecfc66373f46e4934edad2ec
Brian Cristante <brian.cristante@gmail.com>
2019-01-20 02:10:48 +0000

    Merged PR 17: Add System.Text and Wildcard
    Add System.Text and Wildcard
    
    Related work items: #39

---
+++ src/System.Text/src/Wildcard.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace Wildcard
+{
+    public class Wildcard
+    {
+        private string pattern;
+
+        public Wildcard(string pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public bool IsMatch(string text)
+        {
+            bool expandingStar = false;
+
+            int i = 0;
+            int j = 0;
+            while (i < this.pattern.Length)
+            {
+                if (expandingStar)
+                {
+                    // Skip ahead to all possible positions and start matching again
+                    var subpattern = new Wildcard(this.pattern.Substring(i));
+                    return Enumerable.Range(j, text.Length - 1)
+                        .Select(x => text.Substring(x))
+                        .Any(subpattern.IsMatch);
+                }
+                else
+                {
+                    char patternChar = this.pattern[i];
+
+                    if (patternChar == '*')
+                    {
+                        expandingStar = true;
+                        i++;
+                    }
+                    else if (j < text.Length && (patternChar == '?' || patternChar == text[j]))
+                    {
+                        i++;
+                        j++;
+                    }
+                    else
+                    {
+                        // Pattern did not match
+                        return false;
+                    }
+                }
+            }
+
+            // Reached the end of the pattern
+            // Match iff we have evaluated all of the input text
+            // or the last character in the pattern is '*'
+            return j == text.Length || expandingStar;
+        }
+    }
+}
---
+++ src/System.Text/test/WildcardTests.cs
@@ -0,0 +1,90 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Wildcard;
+
+namespace Wildcard.UnitTest
+{
+    [TestClass]
+    public class WildcardTests
+    {
+        [TestMethod]
+        public void EmptyPattern()
+        {
+            Assert.IsTrue(new Wildcard("").IsMatch(""));
+            Assert.IsFalse(new Wildcard("").IsMatch("a"));
+        }
+
+        [TestMethod]
+        public void NoSpecialCharacters()
+        {
+            Assert.IsTrue(new Wildcard("abc").IsMatch("abc"));
+            Assert.IsFalse(new Wildcard("abc").IsMatch("ab"));
+            Assert.IsFalse(new Wildcard("abc").IsMatch("abcd"));
+        }
+
+        [TestMethod]
+        public void JustAsterisk()
+        {
+            Assert.IsTrue(new Wildcard("*").IsMatch(""));
+            Assert.IsTrue(new Wildcard("*").IsMatch("abc"));
+        }
+
+        [TestMethod]
+        public void JustQuestionMark()
+        {
+            Assert.IsFalse(new Wildcard("?").IsMatch(""));
+            Assert.IsTrue(new Wildcard("?").IsMatch("x"));
+        }
+
+        [TestMethod]
+        public void SingleWildcard()
+        {
+            Assert.IsTrue(new Wildcard("a?c").IsMatch("axc"));
+        }
+
+        [TestMethod]
+        public void AsteriskAtEnd()
+        {
+            Assert.IsTrue(new Wildcard("a*").IsMatch("abc"));
+            Assert.IsFalse(new Wildcard("a*").IsMatch("bc"));
+        }
+
+        [TestMethod]
+        public void AsteriskInMiddle()
+        {
+            Assert.IsFalse(new Wildcard("a*b").IsMatch("a"));
+            Assert.IsFalse(new Wildcard("a*b").IsMatch("aa"));
+            Assert.IsTrue(new Wildcard("a*b").IsMatch("ab"));
+            Assert.IsTrue(new Wildcard("a*b").IsMatch("abbbb"));
+            Assert.IsFalse(new Wildcard("a*b").IsMatch("abbbbc"));
+        }
+
+        [TestMethod]
+        public void MultipleAsterisks()
+        {
+            Assert.IsTrue(new Wildcard("*ab*").IsMatch("ab"));
+            Assert.IsTrue(new Wildcard("*ab*").IsMatch("123ab456"));
+
+            Assert.IsTrue(new Wildcard("*/*/*").IsMatch("foo/bar/baz"));
+            Assert.IsTrue(new Wildcard("*/*/*").IsMatch("foo/bar/baz/bash"));
+            Assert.IsTrue(new Wildcard("*/*/*").IsMatch("foo/bar/"));
+            Assert.IsFalse(new Wildcard("*/*/*").IsMatch("foo/bar"));
+        }
+
+        [TestMethod]
+        public void MixedWildcards()
+        {
+            Assert.IsTrue(new Wildcard("?ab*").IsMatch("1ab456"));
+            Assert.IsFalse(new Wildcard("?ab*").IsMatch("ab456"));
+
+            Assert.IsTrue(new Wildcard("ab?*").IsMatch("abc"));
+            Assert.IsFalse(new Wildcard("ab?*").IsMatch("ab"));
+            Assert.IsTrue(new Wildcard("ab?*").IsMatch("abcdef"));
+
+            Assert.IsTrue(new Wildcard("?*/?*/?*").IsMatch("foo/bar/baz"));
+            Assert.IsTrue(new Wildcard("?*/?*/?*").IsMatch("foo/bar/baz/bash"));
+            Assert.IsFalse(new Wildcard("?*/?*/?*").IsMatch("foo/bar/"));
+            Assert.IsFalse(new Wildcard("?*/?*/?*").IsMatch("foo/bar"));
+        }
+    }
+}
