366f8c7e0f2ae03ad37f454b485e7412984d4f73
Brian Cristante <brian.cristante@gmail.com>
2019-06-17 21:52:33 +0000

    Merged PR 36: Add Option type
    Skeleton of Option type

---
+++ src/Recore/src/Option.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Recore
+{
+    // TODO ref struct?
+    public readonly struct Option<T> where T : class
+    {
+        private readonly T value;
+
+        public Option(T value)
+        {
+            this.value = value;
+        }
+
+        public bool HasValue => value != null;
+
+        // TODO can't have this with a ref struct
+        public static Option<T> None = new Option<T>();
+
+        public U Match<U>(Func<T, U> onValue, Func<T, U> onNone)
+        {
+            if (HasValue)
+            {
+                return onValue(value);
+            }
+            else
+            {
+                return onNone(value);
+            }
+        }
+
+        public void Match(Action<T> onValue, Action<T> onNone)
+        {
+            if (HasValue)
+            {
+                onValue(value);
+            }
+            else
+            {
+                onNone(value);
+            }
+        }
+
+        public Option<U> Map<U>(Func<T, U> f) where U : class
+            => Match(
+                x => new Option<U>(f(x)),
+                x => Option<U>.None);
+
+        public Option<U> Bind<U>(Func<T, Option<U>> f) where U : class
+            => Match(
+                f,
+                x => Option<U>.None);
+
+        // TODO Equals
+    }
+}
