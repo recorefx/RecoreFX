8c3d0e3b11dbec81237c101a09d5496c755de4ee
Brian Cristante <brian.cristante@gmail.com>
2019-07-02 02:06:20 +0000

    Merged PR 58: Enhance Wildcard
    * Seal
    * Public `Pattern`
    * Add doc comments

--- src/Recore.Text/src/Wildcard.cs
+++ src/Recore.Text/src/Wildcard.cs
@@ -2,34 +2,50 @@
 
 namespace Recore.Text
 {
-    public class Wildcard
+    /// <summary>
+    /// Represents a text pattern with <c>*</c> and <c>?<c> wildcard characters.
+    /// </summary>
+    /// <remarks>
+    /// In evaluating matches for the pattern, <c>*</c> will match zero or more characters
+    /// while <c>?</c> will match a single character.
+    /// </remarks>
+    public sealed class Wildcard
     {
-        private readonly string pattern;
+        /// <summary>
+        /// Gets the pattern that was passed to the <c cref="Wildcard">Wildcard</c> constructor.
+        /// </summary>
+        public string Pattern { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the <c cref="Wildcard">Wildcard</c> type with the specified pattern.
+        /// </summary>
         public Wildcard(string pattern)
         {
-            this.pattern = pattern;
+            Pattern = pattern;
         }
 
+        /// <summary>
+        /// Determines whether a string matches the wildcard pattern.
+        /// </summary>
         public bool IsMatch(string text)
         {
             bool expandingStar = false;
 
             int i = 0;
             int j = 0;
-            while (i < pattern.Length)
+            while (i < Pattern.Length)
             {
                 if (expandingStar)
                 {
                     // Skip ahead to all possible positions and start matching again
-                    var subpattern = new Wildcard(pattern.Substring(i));
+                    var subpattern = new Wildcard(Pattern.Substring(i));
                     return Enumerable.Range(j, text.Length - 1)
                         .Select(x => text.Substring(x))
                         .Any(subpattern.IsMatch);
                 }
                 else
                 {
-                    char patternChar = pattern[i];
+                    char patternChar = Pattern[i];
 
                     if (patternChar == '*')
                     {
