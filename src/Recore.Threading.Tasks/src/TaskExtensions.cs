e6c8dab588a6659a7535f4a0b9623dc84ebcc5e8
Brian Cristante <brian.cristante@gmail.com>
2019-07-12 02:13:53 +0000

    Merged PR 68: Add remarks to Synchronize()
    Add remarks to Synchronize()

--- src/Recore.Threading.Tasks/src/TaskExtensions.cs
+++ src/Recore.Threading.Tasks/src/TaskExtensions.cs
@@ -12,7 +12,10 @@ namespace Recore.Threading.Tasks
         /// will be rethrown in the current context.
         /// Unlike <c>Task.Wait</c>, that exception will be of its original type,
         /// not <c>AggregateException</c>.
-        // Also, it will preserve its original stack trace.
+        /// It will also preserve its original stack trace.
+        /// This exception-throwing behavior is the same as if you had used <c>await</c>.
+        /// Note that it is still possible to deadlock with this method.
+        /// See https://blog.stephencleary.com/2014/12/a-tour-of-task-part-6-results.html.
         /// </remarks>
         public static void Synchronize(this Task task) => task.GetAwaiter().GetResult();
 
@@ -24,7 +27,10 @@ namespace Recore.Threading.Tasks
         /// will be rethrown in the current context.
         /// Unlike <c>Task&lt;T&gt;.Result</c>, that exception will be of its original type,
         /// not <c>AggregateException</c>.
-        /// Also, it will preserve its original stack trace.
+        /// It will also preserve its original stack trace.
+        /// This exception-throwing behavior is the same as if you had used <c>await</c>.
+        /// Note that it is still possible to deadlock with this method.
+        /// See https://blog.stephencleary.com/2014/12/a-tour-of-task-part-6-results.html.
         /// </remarks>
         public static T Synchronize<T>(this Task<T> task) => task.GetAwaiter().GetResult();
     }
