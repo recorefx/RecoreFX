0fc6e1caeda729b612bbbeeeb95a0f75426fae1d
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2019-07-31 23:33:53 -0400

    Add Of (#4)
    * Add Of
    
    * Bump version

---
+++ src/Recore/Of.cs
@@ -0,0 +1,58 @@
+using System;
+
+namespace Recore
+{
+    /// <summary>
+    /// Abstract base class for defining types that alias an existing type.
+    /// </summary>
+    /// <example>
+    /// Use <c cref="Of{T}">Of</c> to create a strongly-typed "alias" of another type.
+    /// <code>
+    /// class Address : Of&lt;string&gt; {}
+    /// var address = new Address { Value = "1 Microsoft Way" };
+    /// Console.WriteLine(address); // prints "1 Microsoft Way"
+    /// </code>
+    /// </example>
+    public abstract class Of<T> : IEquatable<Of<T>>
+    {
+        /// <summary>
+        /// The underlying instance of the wrapped type.
+        /// </summary>
+        public T Value { get; set; }
+
+        /// <summary>
+        /// Returns the string representation for the underlying object.
+        /// </summary>
+        public override string ToString() => Value.ToString();
+
+        /// <summary>
+        /// Determines whether this instance is equal to another object.
+        /// </summary>
+        public override bool Equals(object obj)
+            => obj is Of<T> && Equals((Of<T>)obj);
+
+        /// <summary>
+        /// Determines whether two instances of the type are equal.
+        /// </summary>
+        public bool Equals(Of<T> other)
+            => other != null
+            && Equals(Value, other.Value);
+
+        /// <summary>
+        /// Returns the hash code for the underlying object.
+        /// </summary>
+        public override int GetHashCode() => Value.GetHashCode();
+
+        /// <summary>
+        /// Determines whether two instances of the type are equal.
+        /// </summary>
+        public static bool operator ==(Of<T> lhs, Of<T> rhs)
+            => Equals(lhs, rhs);
+
+        /// <summary>
+        /// Determines whether two instances of the type are not equal.
+        /// </summary>
+        public static bool operator !=(Of<T> lhs, Of<T> rhs)
+            => !Equals(lhs, rhs);
+    }
+}
