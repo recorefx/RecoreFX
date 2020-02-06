0103de0f068703982bc92418a71313dae9baa645
Brian Cristante <brian.cristante@gmail.com>
2020-02-05 22:35:19 -0500

    Check args for null in Lift

--- src/Recore.Functional/Lift.cs
+++ src/Recore.Functional/Lift.cs
@@ -15,15 +15,23 @@ namespace Recore.Functional
         /// Converts a unary function to work with <c cref="Optional{T}">Optional</c>.
         /// </summary>
         public static Func<Optional<T>, Optional<TResult>> OnOptional<T, TResult>(Func<T, TResult> func)
-            => optional
-            => optional.OnValue(func);
+        {
+            if (func == null)
+                throw new ArgumentNullException(nameof(func));
+
+            return optional => optional.OnValue(func);
+        }
 
         /// <summary>
         /// Converts a unary action to work with <c cref="Optional{T}">Optional</c>.
         /// </summary>
         public static Action<Optional<T>> OnOptional<T>(Action<T> action)
-            => optional
-            => optional.IfValue(action);
+        {
+            if (action == null)
+                throw new ArgumentNullException(nameof(action));
+
+            return optional => optional.IfValue(action);
+        }
 
         /// <summary>
         /// Combines two unary functions with the same return type
@@ -32,10 +40,14 @@ namespace Recore.Functional
         public static Func<Either<TLeft, TRight>, TResult> OnEither<TLeft, TRight, TResult>(
             Func<TLeft, TResult> leftFunc,
             Func<TRight, TResult> rightFunc)
-            => either
-            => either.Switch(
-                leftFunc,
-                rightFunc);
+        {
+            if (leftFunc == null)
+                throw new ArgumentNullException(nameof(leftFunc));
+            if (rightFunc == null)
+                throw new ArgumentNullException(nameof(rightFunc));
+
+            return either => either.Switch(leftFunc, rightFunc);
+        }
 
         /// <summary>
         /// Combines two unary actions into a single action taking either of their parameters.
@@ -43,23 +55,35 @@ namespace Recore.Functional
         public static Action<Either<TLeft, TRight>> OnEither<TLeft, TRight>(
             Action<TLeft> leftAction,
             Action<TRight> rightAction)
-            => either
-            => either.Switch(
-                leftAction,
-                rightAction);
+        {
+            if (leftAction == null)
+                throw new ArgumentNullException(nameof(leftAction));
+            if (rightAction == null)
+                throw new ArgumentNullException(nameof(rightAction));
+
+            return either => either.Switch(leftAction, rightAction);
+        }
 
         /// <summary>
         /// Converts a function operating on a scalar value to a function operating on a sequence of values.
         /// </summary>
         public static Func<IEnumerable<T>, IEnumerable<U>> OnEnumerable<T, U>(Func<T, U> func)
-            => enumerable
-            => enumerable.Select(func);
+        {
+            if (func == null)
+                throw new ArgumentNullException(nameof(func));
+
+            return enumerable.Select(func);
+        }
 
         /// <summary>
         /// Converts an action operating on a scalar value to an action operating on a sequence of values.
         /// </summary>
         public static Action<IEnumerable<T>> OnEnumerable<T, U>(Action<T> action)
-            => enumerable
-            => enumerable.ForEach(action);
+        {
+            if (action == null)
+                throw new ArgumentNullException(nameof(action));
+
+            return enumerable => enumerable.ForEach(action);
+        }
     }
 }
