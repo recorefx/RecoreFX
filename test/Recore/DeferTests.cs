bfed6f1882a67be257ef7f489644ad53a869f28e
Brian Cristante <brian.cristante@gmail.com>
2020-02-11 18:50:59 -0500

    Don't dispose Defer in its finalizer

--- test/Recore/DeferTests.cs
+++ test/Recore/DeferTests.cs
@@ -7,11 +7,10 @@ namespace Recore.Tests
     public class DeferTests
     {
         [Fact]
-        public void InvokesCallbackOnDispose()
+        public void InvokesCallbackWhenDisposed()
         {
             bool called = false;
-
-            using (var defer = new Defer(() => { called = true; }))
+            using (var defer = new Defer(() => called = true))
             {
                 Assert.False(called);
             }
@@ -43,6 +42,30 @@ namespace Recore.Tests
 
             defer.Dispose();
             Assert.True(called);
+        }
+
+        [Fact]
+        public void DoesNotInvokeCallbackWhenFinalized()
+        {
+            bool called = false;
+
+            // Apparently, you need method scope for the GC to consider an object dead.
+            // A normal code block won't suffice.
+            void TestMethod()
+            {
+                var defer = new Defer(() => called = true);
+                Assert.False(called);
+            }
+
+            TestMethod();
+
+            // Even though GC.Collect() is supposed to block until GC is done,
+            // it seems that you have to wait on the finalizers as well.
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+            Assert.False(called);
         }
     }
 }
