6cd71885e9a94b7eb0dbeb844649d898d94250c9
Brian Cristante <brian.cristante@gmail.com>
2020-02-10 22:43:13 -0500

    Use <see> tag in doc comments

--- src/Recore.Collections.Generic/IReadOnlyDictionaryExtensions.cs
+++ src/Recore.Collections.Generic/IReadOnlyDictionaryExtensions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 namespace Recore.Collections.Generic
 {
     /// <summary>
-    /// Provides additional methods for working with <c cref="IReadOnlyDictionary{TKey, TValue}">IReadOnlyDictionary</c>.
+    /// Provides additional methods for working with <see cref="IReadOnlyDictionary{TKey, TValue}" />.
     /// </summary>
     public static class IReadOnlyDictionaryExtensions
     {
--- src/Recore.Linq/ToLinkedList.cs
+++ src/Recore.Linq/ToLinkedList.cs
@@ -1,22 +1,22 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Recore.Linq
 {
     public static partial class Renumerable
     {
         /// <summary>
-        /// Creates a <c cref="LinkedList{T}">LinkedList&lt;T&gt;</c>
-        /// from an <c cref="IEnumerable{T}">IEnumerable&lt;T&gt;</c>.
+        /// Creates a <see cref="LinkedList{T}" /> from an <see cref="IEnumerable{T}" />.
         /// </summary>
         /// <remarks>
         /// Linked lists don't need to be resized when adding elements,
-        /// which can give this method better performance than <c>ToList</c> or <c>ToArray</c>.
+        /// which can give this method better performance than <see cref="Enumerable.ToList{T}(IEnumerable{T})" /> or <see cref="Enumerable.ToArray{T}(IEnumerable{T})" />.
         /// A common case is when you just want to force eager evaluation of a series of
-        /// operations on an <c cref="IEnumerable{T}">IEnumerable&lt;T&gt;</c>
+        /// operations on an <see cref="IEnumerable{T}" />
         /// or when you want to cache elements when performing multiple enumerations.
         /// For these cases, you don't need random access to elements, which makes
-        /// <c cref="LinkedList{T}">LinkedList&lt;T&gt;</c> a suitable data structure for storing the elements.
+        /// <see cref="LinkedList{T}" /> a suitable data structure for storing the elements.
         /// </remarks>
         public static LinkedList<TSource> ToLinkedList<TSource>(this IEnumerable<TSource> source)
         {
@@ -30,6 +30,7 @@ namespace Recore.Linq
             {
                 result.AddLast(item);
             }
+
             return result;
         }
     }
