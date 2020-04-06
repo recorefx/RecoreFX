b746e7fdb2d32726bb5b546175265bfeb0f86678
Brian Cristante <brian.cristante@gmail.com>
2020-04-05 21:55:44 -0400

    Fix overload resolution for IReadOnlyDictionary

---
+++ test/Recore.Collections.Generic/IReadOnlyDictionaryExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+using Xunit;
+
+namespace Recore.Collections.Generic.Tests
+{
+    public class IReadOnlyDictionaryExtensionsTests
+    {
+        [Fact]
+        public void ValueOrDefault_HasValue()
+        {
+            var dictionary = new ReadOnlyDictionary<string, int>(new Dictionary<string, int>
+            {
+                ["abc"] = 12
+            });
+
+            Assert.Equal(12, dictionary.ValueOrDefault("abc"));
+        }
+    }
+}
