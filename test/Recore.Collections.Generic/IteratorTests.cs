531c24ed3754ed645918085370a2abda947b31c8
Brian Cristante <brian.cristante@gmail.com>
2020-04-08 21:18:13 -0400

    Add IterateOverCollection

--- test/Recore.Collections.Generic/IteratorTests.cs
+++ test/Recore.Collections.Generic/IteratorTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 
 using Xunit;
@@ -13,6 +14,29 @@ namespace Recore.Collections.Generic.Tests
             var iterator = Iterator.FromEnumerable(empty);
 
             Assert.False(iterator.HasNext);
+        }
+
+        [Fact]
+        public void IterateOverCollection()
+        {
+            var strings = new[]
+            {
+                "hello",
+                "world",
+                "goodbye"
+            };
+
+            var iterator = Iterator.FromEnumerable(strings);
+
+            var count = 0;
+            while (iterator.HasNext)
+            {
+                Assert.Equal(strings[count++], iterator.Next());
+            }
+
+            // Make sure we iterated over the whole collection.
+            Assert.Equal(strings.Length, count);
+            Assert.Throws<InvalidOperationException>(() => iterator.Next());
         }
     }
 }
