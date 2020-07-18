032257602411abeeaaaff9a27bf7da704c1681e8
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-07-18 10:41:17 -0400

    Rename Wildcard -> Glob through files

--- test/Recore.Text/GlobTests.cs
+++ test/Recore.Text/GlobTests.cs
@@ -7,81 +7,81 @@ namespace Recore.Text.Tests
         [Fact]
         public void EmptyPattern()
         {
-            Assert.True(new Wildcard("").IsMatch(""));
-            Assert.False(new Wildcard("").IsMatch("a"));
+            Assert.True(new Glob("").IsMatch(""));
+            Assert.False(new Glob("").IsMatch("a"));
         }
 
         [Fact]
         public void NoSpecialCharacters()
         {
-            Assert.True(new Wildcard("abc").IsMatch("abc"));
-            Assert.False(new Wildcard("abc").IsMatch("ab"));
-            Assert.False(new Wildcard("abc").IsMatch("abcd"));
+            Assert.True(new Glob("abc").IsMatch("abc"));
+            Assert.False(new Glob("abc").IsMatch("ab"));
+            Assert.False(new Glob("abc").IsMatch("abcd"));
         }
 
         [Fact]
         public void JustAsterisk()
         {
-            Assert.True(new Wildcard("*").IsMatch(""));
-            Assert.True(new Wildcard("*").IsMatch("abc"));
+            Assert.True(new Glob("*").IsMatch(""));
+            Assert.True(new Glob("*").IsMatch("abc"));
         }
 
         [Fact]
         public void JustQuestionMark()
         {
-            Assert.False(new Wildcard("?").IsMatch(""));
-            Assert.True(new Wildcard("?").IsMatch("x"));
+            Assert.False(new Glob("?").IsMatch(""));
+            Assert.True(new Glob("?").IsMatch("x"));
         }
 
         [Fact]
         public void SingleWildcard()
         {
-            Assert.True(new Wildcard("a?c").IsMatch("axc"));
+            Assert.True(new Glob("a?c").IsMatch("axc"));
         }
 
         [Fact]
         public void AsteriskAtEnd()
         {
-            Assert.True(new Wildcard("a*").IsMatch("abc"));
-            Assert.False(new Wildcard("a*").IsMatch("bc"));
+            Assert.True(new Glob("a*").IsMatch("abc"));
+            Assert.False(new Glob("a*").IsMatch("bc"));
         }
 
         [Fact]
         public void AsteriskInMiddle()
         {
-            Assert.False(new Wildcard("a*b").IsMatch("a"));
-            Assert.False(new Wildcard("a*b").IsMatch("aa"));
-            Assert.True(new Wildcard("a*b").IsMatch("ab"));
-            Assert.True(new Wildcard("a*b").IsMatch("abbbb"));
-            Assert.False(new Wildcard("a*b").IsMatch("abbbbc"));
+            Assert.False(new Glob("a*b").IsMatch("a"));
+            Assert.False(new Glob("a*b").IsMatch("aa"));
+            Assert.True(new Glob("a*b").IsMatch("ab"));
+            Assert.True(new Glob("a*b").IsMatch("abbbb"));
+            Assert.False(new Glob("a*b").IsMatch("abbbbc"));
         }
 
         [Fact]
         public void MultipleAsterisks()
         {
-            Assert.True(new Wildcard("*ab*").IsMatch("ab"));
-            Assert.True(new Wildcard("*ab*").IsMatch("123ab456"));
+            Assert.True(new Glob("*ab*").IsMatch("ab"));
+            Assert.True(new Glob("*ab*").IsMatch("123ab456"));
 
-            Assert.True(new Wildcard("*/*/*").IsMatch("foo/bar/baz"));
-            Assert.True(new Wildcard("*/*/*").IsMatch("foo/bar/baz/bash"));
-            Assert.True(new Wildcard("*/*/*").IsMatch("foo/bar/"));
-            Assert.False(new Wildcard("*/*/*").IsMatch("foo/bar"));
+            Assert.True(new Glob("*/*/*").IsMatch("foo/bar/baz"));
+            Assert.True(new Glob("*/*/*").IsMatch("foo/bar/baz/bash"));
+            Assert.True(new Glob("*/*/*").IsMatch("foo/bar/"));
+            Assert.False(new Glob("*/*/*").IsMatch("foo/bar"));
         }
 
         [Fact]
         public void MixedWildcards()
         {
-            Assert.True(new Wildcard("?ab*").IsMatch("1ab456"));
-            Assert.False(new Wildcard("?ab*").IsMatch("ab456"));
+            Assert.True(new Glob("?ab*").IsMatch("1ab456"));
+            Assert.False(new Glob("?ab*").IsMatch("ab456"));
 
-            Assert.True(new Wildcard("ab?*").IsMatch("abc"));
-            Assert.False(new Wildcard("ab?*").IsMatch("ab"));
-            Assert.True(new Wildcard("ab?*").IsMatch("abcdef"));
+            Assert.True(new Glob("ab?*").IsMatch("abc"));
+            Assert.False(new Glob("ab?*").IsMatch("ab"));
+            Assert.True(new Glob("ab?*").IsMatch("abcdef"));
 
-            Assert.True(new Wildcard("?*/?*/?*").IsMatch("foo/bar/baz"));
-            Assert.True(new Wildcard("?*/?*/?*").IsMatch("foo/bar/baz/bash"));
-            Assert.False(new Wildcard("?*/?*/?*").IsMatch("foo/bar/"));
-            Assert.False(new Wildcard("?*/?*/?*").IsMatch("foo/bar"));
+            Assert.True(new Glob("?*/?*/?*").IsMatch("foo/bar/baz"));
+            Assert.True(new Glob("?*/?*/?*").IsMatch("foo/bar/baz/bash"));
+            Assert.False(new Glob("?*/?*/?*").IsMatch("foo/bar/"));
+            Assert.False(new Glob("?*/?*/?*").IsMatch("foo/bar"));
         }
     }
 }
