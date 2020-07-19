685e33d393e9d61dbb86b2c820fffea514e79229
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-07-19 10:56:10 -0400

    Add some basic FsCheck tests to CiphertextTests

--- test/Recore.Security.Cryptography/CiphertextTests.cs
+++ test/Recore.Security.Cryptography/CiphertextTests.cs
@@ -1,5 +1,8 @@
 using System;
 using System.Security.Cryptography;
+
+using FsCheck;
+using FsCheck.Xunit;
 using Xunit;
 
 namespace Recore.Security.Cryptography.Tests
@@ -126,6 +129,69 @@ namespace Recore.Security.Cryptography.Tests
                 ciphertext1 == withSalt);
             Assert.True(
                 ciphertext1 != withSalt);
+        }
+
+        [Property]
+        public Property MD5HelperAlwaysEqualToLongForm()
+        {
+            return Prop.ForAll((string plaintext) =>
+            {
+                if (plaintext == null)
+                {
+                    // Skip
+                    return true;
+                }
+
+                using (var md5 = MD5.Create())
+                {
+                    var salt = Array.Empty<byte>();
+                    var longForm = Ciphertext<MD5>.Encrypt(plaintext, salt, md5);
+                    var shortForm = Ciphertext.MD5(plaintext, salt);
+                    return longForm == shortForm;
+                }
+            });
+        }
+
+        [Property]
+        public Property SHA1HelperAlwaysEqualToLongForm()
+        {
+            return Prop.ForAll((string plaintext) =>
+            {
+                if (plaintext == null)
+                {
+                    // Skip
+                    return true;
+                }
+
+                using (var sha1 = SHA1.Create())
+                {
+                    var salt = Array.Empty<byte>();
+                    var longForm = Ciphertext<SHA1>.Encrypt(plaintext, salt, sha1);
+                    var shortForm = Ciphertext.SHA1(plaintext, salt);
+                    return longForm == shortForm;
+                }
+            });
+        }
+
+        [Property]
+        public Property SHA256HelperAlwaysEqualToLongForm()
+        {
+            return Prop.ForAll((string plaintext) =>
+            {
+                if (plaintext == null)
+                {
+                    // Skip
+                    return true;
+                }
+
+                using (var sha256 = SHA256.Create())
+                {
+                    var salt = Array.Empty<byte>();
+                    var longForm = Ciphertext<SHA256>.Encrypt(plaintext, salt, sha256);
+                    var shortForm = Ciphertext.SHA256(plaintext, salt);
+                    return longForm == shortForm;
+                }
+            });
         }
     }
 }
