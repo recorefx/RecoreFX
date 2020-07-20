3bea978c9e6c21e03e7d8b5cf4487b19019d062a
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-07-20 18:08:23 -0400

    Add more links in the docs (#78)

--- src/Recore.Linq/Renumerable.cs
+++ src/Recore.Linq/Renumerable.cs
@@ -4,7 +4,7 @@ namespace Recore.Linq
 {
     /// <summary>
     /// Provides additional methods for working with objects that implement
-    /// <c cref="IEnumerable{T}">IEnumerable&lt;T&gt;</c>.
+    /// <see cref="IEnumerable{T}"/>.
     /// </summary>
     public static partial class Renumerable
     {
