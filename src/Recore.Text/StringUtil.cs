af4995d6d0e61b3bca535f93ad9fd3463b14a864
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-07-20 17:32:46 -0400

    Restore StringUtil

---
+++ src/Recore.Text/StringUtil.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+
+namespace Recore.Text
+{
+    /// <summary>
+    /// Contains helper methods for working with strings.
+    /// </summary>
+    public static class StringUtil
+    {
+        /// <summary>
+        /// Concatenates a sequence of strings into a single string
+        /// where each input string is separated by <see cref="Environment.NewLine"/>.
+        /// </summary>
+        public static string JoinLines(IEnumerable<string> lines)
+        {
+            if (lines == null)
+            {
+                throw new ArgumentNullException(nameof(lines));
+            }
+
+            return string.Join(Environment.NewLine, lines);
+        }
+
+        /// <summary>
+        /// Concatenates a variable number of string arguments into a single string
+        /// where each input string is separated by <see cref="Environment.NewLine"/>.
+        /// </summary>
+        public static string JoinLines(params string[] lines)
+            => JoinLines((IEnumerable<string>)lines);
+    }
+}
