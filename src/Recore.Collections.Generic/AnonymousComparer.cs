aa99c6732a4d3c6061fe0c64c98b9a1731c4c466
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-07-25 22:24:34 -0400

    Fix build

--- src/Recore.Collections.Generic/AnonymousComparer.cs
+++ src/Recore.Collections.Generic/AnonymousComparer.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 
 namespace Recore.Collections.Generic
 {
+    /// <summary>
+    /// Compares instances of a type using the given comparison function.
+    /// </summary>
     public sealed class AnonymousComparer<T> : IComparer<T>
     {
         private readonly Func<T, T, int> compare;
 
+        /// <summary>
+        /// Creates an instance of <see cref="AnonymousComparer{T}"/>.
+        /// </summary>
         public AnonymousComparer(Func<T, T, int> compare)
         {
             this.compare = compare;
         }
 
+        /// <summary>
+        /// Invokes the given comparison function on two objects.
+        /// </summary>
         public int Compare(T x, T y) => compare(x, y);
     }
 }
--- src/Recore.Collections.Generic/AnonymousEqualityComparer.cs
+++ src/Recore.Collections.Generic/AnonymousEqualityComparer.cs
@@ -3,18 +3,33 @@ using System.Collections.Generic;
 
 namespace Recore.Collections.Generic
 {
+    /// <summary>
+    /// Compares instances of a type using the given equality function.
+    /// </summary>
     public sealed class AnonymousEqualityComparer<T> : IEqualityComparer<T>
     {
         private readonly Func<T, T, bool> equals;
         private readonly Func<T, int> getHashCode;
 
+        /// <summary>
+        /// Creates an instance of <see cref="AnonymousEqualityComparer{T}"/>.
+        /// </summary>
         public AnonymousEqualityComparer(Func<T, T, bool> equals, Func<T, int> getHashCode)
         {
             this.equals = equals;
             this.getHashCode = getHashCode;
         }
 
+
+        /// <summary>
+        /// Invokes the given comparison function on two objects.
+        /// </summary>
         public bool Equals(T x, T y) => equals(x, y);
 
+
+        /// <summary>
+        /// Invokes the given hashing function on an object.
+        /// </summary>
         public int GetHashCode(T obj) => getHashCode(obj);
+    }
 }
