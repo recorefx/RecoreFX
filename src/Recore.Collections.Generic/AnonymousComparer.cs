6692e3967a67aef0662da6b9b5854444d0f58687
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-07-25 22:15:57 -0400

    Add AnonymousComparer and AnonymousEqualityComparer

---
+++ src/Recore.Collections.Generic/AnonymousComparer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Recore.Collections.Generic
+{
+    public sealed class AnonymousComparer<T> : IComparer<T>
+    {
+        private readonly Func<T, T, int> compare;
+
+        public AnonymousComparer(Func<T, T, int> compare)
+        {
+            this.compare = compare;
+        }
+
+        public int Compare(T x, T y) => compare(x, y);
+    }
+}
---
+++ src/Recore.Collections.Generic/AnonymousEqualityComparer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Recore.Collections.Generic
+{
+    public sealed class AnonymousEqualityComparer<T> : IEqualityComparer<T>
+    {
+        private readonly Func<T, T, bool> equals;
+        private readonly Func<T, int> getHashCode;
+
+        public AnonymousEqualityComparer(Func<T, T, bool> equals, Func<T, int> getHashCode)
+        {
+            this.equals = equals;
+            this.getHashCode = getHashCode;
+        }
+
+        public bool Equals(T x, T y) => equals(x, y);
+
+        public int GetHashCode(T obj) => getHashCode(obj);
+}
