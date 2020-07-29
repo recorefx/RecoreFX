be53d09ec640860f732cffa02e80b5db5144a44c
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-07-28 21:46:10 -0400

    More test cases for CanConvert

--- test/Recore.Text.Json.Serialization.Converters/OfConverterTests.cs
+++ test/Recore.Text.Json.Serialization.Converters/OfConverterTests.cs
@@ -1,10 +1,15 @@
-﻿using Xunit;
+﻿using System.Security.Cryptography;
+using Xunit;
+
+using Recore.Security.Cryptography;
 
 namespace Recore.Text.Json.Serialization.Converters.Tests
 {
-    public class OfConverterTests
+    public class OfConverterFactoryTests
     {
+        private enum SomeEnum { Yes, No }
         private class DerivedOfString : Of<string> { }
+        private class DerivedDerivedOfString : Of<string> { }
 
         [Fact]
         public void CanConvert()
@@ -13,7 +18,13 @@ namespace Recore.Text.Json.Serialization.Converters.Tests
 
             Assert.True(factory.CanConvert(typeof(Of<>)));
             Assert.True(factory.CanConvert(typeof(Of<string>)));
+            Assert.True(factory.CanConvert(typeof(Of<int>)));
+            Assert.True(factory.CanConvert(typeof(Of<SomeEnum>)));
+            Assert.True(factory.CanConvert(typeof(Of<Of<object>>)));
+            Assert.True(factory.CanConvert(typeof(Token)));
+            Assert.True(factory.CanConvert(typeof(Ciphertext<SHA1>)));
             Assert.True(factory.CanConvert(typeof(DerivedOfString)));
+            Assert.True(factory.CanConvert(typeof(DerivedDerivedOfString)));
 
             Assert.False(factory.CanConvert(typeof(string)));
             Assert.False(factory.CanConvert(typeof(object)));
