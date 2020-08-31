87f8e86b17e0dd3ed06e496dcf8a18349ccd6bf9
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-08-30 20:22:54 -0400

    Add FsCheck tests to ProductTests

--- test/Recore.Linq/ProductTests.cs
+++ test/Recore.Linq/ProductTests.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+using FsCheck;
+using FsCheck.Xunit;
 using Xunit;
 
 namespace Recore.Linq.Tests
@@ -99,11 +101,63 @@ namespace Recore.Linq.Tests
                 empty.Product(empty).ToArray());
         }
 
-        // FsCheck properties:
-        // - product with empty -> empty
-        // - product with length 1 -> same length
-        // - product length m with length n -> length m * n
-        // - each element of original sequence in new sequence
-        // - a.Product(b).SortBy(x => x.first) == b.Product(a).Select(Swap).SortBy(x => x.first)
+        // Product length m with length n -> length m * n
+        [Property]
+        public Property ProductLengthEqualsProductOfInputLengths()
+        {
+            return Prop.ForAll((List<int> first, List<string> second) =>
+            {
+                if (first is null || second is null)
+                {
+                    // Skip
+                    return true;
+                }
+
+                var product = first.Product(second).ToList();
+                return product.Count == (first.Count * second.Count);
+            });
+        }
+
+        // Each element of original sequence in new sequence
+        [Property]
+        public Property ResultContainsAllInputElements()
+        {
+            return Prop.ForAll((List<int> first, List<string> second) =>
+            {
+                if (first is null || second is null)
+                {
+                    // Skip
+                    return true;
+                }
+
+                if (first.Count == 0 || second.Count == 0)
+                {
+                    // Skip
+                    return true;
+                }
+
+                var product = first.Product(second).ToArray();
+
+                var firsts = product.Select(x => x.first).ToHashSet();
+                foreach (var item in first)
+                {
+                    if (!firsts.Contains(item))
+                    {
+                        return false;
+                    }
+                }
+
+                var seconds = product.Select(x => x.second).ToHashSet();
+                foreach (var item in second)
+                {
+                    if (!seconds.Contains(item))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            });
+        }
     }
 }
