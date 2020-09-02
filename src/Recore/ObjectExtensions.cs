0e50c8f3052350d4aa598cbe786e89e504c30723
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-09-02 12:17:26 -0400

    Add example for StaticCast

--- src/Recore/ObjectExtensions.cs
+++ src/Recore/ObjectExtensions.cs
@@ -13,8 +13,23 @@ namespace Recore
         /// <remarks>
         /// This method is analogous to <c>static_cast</c> in C++.
         /// In C#, casting is normally an unsafe operation: a failed cast will throw <see cref="InvalidCastException"/>.
-        /// On the other hand, <see cref="StaticCast{T}"/> will never throw at runtime if it compiles.
+        /// On the other hand, <see cref="StaticCast{T}"/> will never throw at run time if it compiles.
         /// </remarks>
+        /// <example>
+        /// Useful for overcoming those pesky variance issues:
+        /// <code>
+        /// Task&lt;IEnumerable&lt;object&gt;&gt; GetObjectsAsync()
+        /// {
+        ///     var result = new[] { "hello" };
+        ///     
+        ///     // error CS0029 because the static type of `result` is an array, not an `IEnumerable`
+        ///     // return Task.FromResult(result);
+        /// 
+        ///     // 👍
+        ///     return Task.FromResult(result.StaticCast&lt;IEnumerable&lt;object&gt;&gt;());
+        /// }
+        /// </code>
+        /// </example>
         public static T StaticCast<T>(this T obj) => obj;
     }
 }
