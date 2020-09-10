6b34c1c336b5a5bfb4b698d2e1ab7a3d1730d0fc
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-09-10 10:22:14 -0400

    Capitalize tuple field names

--- src/Recore.Linq/Enumerate.cs
+++ src/Recore.Linq/Enumerate.cs
@@ -8,7 +8,7 @@ namespace Recore.Linq
         /// <summary>
         /// Returns each element from a sequence along with its number from the beginning of the sequence, starting from zero.
         /// </summary>
-        public static IEnumerable<(int index, TSource item)> Enumerate<TSource>(this IEnumerable<TSource> source)
+        public static IEnumerable<(int Index, TSource Item)> Enumerate<TSource>(this IEnumerable<TSource> source)
             => Enumerable.Range(0, int.MaxValue).Zip(source);
     }
 }
