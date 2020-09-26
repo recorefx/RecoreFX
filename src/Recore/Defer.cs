f54360cf3f55012d7cc264f321e5aca7cd1ea51b
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-09-26 10:42:48 -0400

    add AsyncDeferTests

--- src/Recore/Defer.cs
+++ src/Recore/Defer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 
 namespace Recore
 {
@@ -9,7 +10,7 @@ namespace Recore
     /// Not thread-safe.
     /// Concurrent calls to dispose the object may result in the action being invoked
     /// multiple times.
-    /// However, synchronous calls to <see cref="Dispose"/> are idempotent.
+    /// However, sequential calls to <see cref="Dispose"/> are idempotent.
     /// If an instance of this type is created and never disposed, the callback will not be called.
     /// By design, the callback is not called from the finalizer, which would happen non-determinstically.
     /// </remarks>
@@ -42,6 +43,50 @@ namespace Recore
             }
 
             action();
+            isDisposed = true;
+        }
+    }
+
+    /// <summary>
+    /// Invokes an asynchronous action when disposed.
+    /// </summary>
+    /// <remarks>
+    /// Not thread-safe.
+    /// Concurrent calls to dispose the object may result in the action being invoked
+    /// multiple times.
+    /// However, sequential calls to <see cref="DisposeAsync"/> are idempotent.
+    /// If an instance of this type is created and never disposed, the callback will not be called.
+    /// By design, the callback is not called from the finalizer, which would happen non-determinstically.
+    /// </remarks>
+    public sealed class AsyncDefer : IAsyncDisposable
+    {
+        private readonly AsyncAction action;
+        private bool isDisposed = false;
+
+        /// <summary>
+        /// Initializes an object with an action to invoke when the object is disposed.
+        /// </summary>
+        public AsyncDefer(AsyncAction action)
+        {
+            if (action is null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            this.action = action;
+        }
+
+        /// <summary>
+        /// Invokes the callback registered with the object and marks the object as disposed.
+        /// </summary>
+        public async ValueTask DisposeAsync()
+        {
+            if (isDisposed)
+            {
+                return;
+            }
+
+            await action();
             isDisposed = true;
         }
     }
