57049fc6837038d1176ee743f4373d3df5c92e21
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-09-26 13:03:46 -0400

    Add ToAsyncEnumerable (#180)
    * Add ToAsyncEnumerable
    
    * Add to changelog
    
    * Add tests

---
+++ src/Recore.Linq/ToAsyncEnumerable.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Recore.Linq
+{
+    public static partial class Renumerable
+    {
+        /// <summary>
+        /// Converts an asynchronously-returned sequence of <typeparamref name="T"/> to <see cref="IAsyncEnumerable{T}"/>.
+        /// </summary>
+        /// <remarks>
+        /// The <see cref="IAsyncEnumerable{T}"/> returned by this method won't yield the same throughput improvement of
+        /// asynchronously generating each element in the sequence.
+        /// Rather, this is a shim to help more methods work with <see cref="IAsyncEnumerable{T}"/>
+        /// and encourage its use in a codebase.
+        /// </remarks>
+        public static async IAsyncEnumerable<T> ToAsyncEnumerable<T>(this Task<IEnumerable<T>> source)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            foreach (var item in await source)
+            {
+                yield return item;
+            }
+        }
+
+        /// <summary>
+        /// Converts an sequence of tasks to <see cref="IAsyncEnumerable{T}"/>.
+        /// </summary>
+        /// <remarks>
+        /// This is a shim to help more methods work with <see cref="IAsyncEnumerable{T}"/>
+        /// and encourage its use in a codebase.
+        /// </remarks>
+        public static async IAsyncEnumerable<T> ToAsyncEnumerable<T>(this IEnumerable<Task<T>> source)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            foreach (var item in source)
+            {
+                yield return await item;
+            }
+        }
+    }
+}
