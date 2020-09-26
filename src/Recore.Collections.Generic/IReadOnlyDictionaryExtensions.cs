414ab807daad87addf7ddbff52341880b8a6b2fe
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-09-25 21:46:46 -0400

    Remove IReadOnlyDictionaryExtensions.GetValueOrDefault()

--- src/Recore.Collections.Generic/IReadOnlyDictionaryExtensions.cs
+++ src/Recore.Collections.Generic/IReadOnlyDictionaryExtensions.cs
@@ -11,24 +11,8 @@ namespace Recore.Collections.Generic
         /// <summary>
         /// Gets the value that is associated with the specific key or the default value for the type <typeparamref name="TValue"/>.
         /// </summary>
-        public static TValue? GetValueOrDefault<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dict, TKey key)
-        where TKey : notnull
-        {
-            if (dict.TryGetValue(key, out TValue value))
-            {
-                return value;
-            }
-            else
-            {
-                return default;
-            }
-        }
-
-        /// <summary>
-        /// Gets the value that is associated with the specific key or the default value for the type <typeparamref name="TValue"/>.
-        /// </summary>
         /// <remarks>
-        /// This is duplicated from <see cref="GetValueOrDefault{TKey, TValue}(IReadOnlyDictionary{TKey, TValue}, TKey)"/>
+        /// This is duplicated from <see cref="CollectionExtensions.GetValueOrDefault{TKey, TValue}(IReadOnlyDictionary{TKey, TValue}, TKey)"/>
         /// in order to resolve the compile-time ambiguity between that method and <see cref="IDictionaryExtensions.GetValueOrDefault{TKey, TValue}(IDictionary{TKey, TValue}, TKey)"/>
         /// for instances of <see cref="ReadOnlyDictionary{TKey, TValue}"/>.
         /// </remarks>
