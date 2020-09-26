87e50503266642d5d14142c98a936f6b2f39efa2
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-09-26 10:50:45 -0400

    Add AsyncDefer (#177)

--- test/Recore/DeferTests.cs
+++ test/Recore/DeferTests.cs
@@ -1,5 +1,5 @@
 using System;
-
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Recore.Tests
@@ -10,7 +10,7 @@ namespace Recore.Tests
         public void InvokesCallbackWhenDisposed()
         {
             bool called = false;
-            using (var defer = new Defer(() => called = true))
+            using (new Defer(() => called = true))
             {
                 Assert.False(called);
             }
@@ -53,6 +53,68 @@ namespace Recore.Tests
             Func.Invoke(Unit.Close(() =>
             {
                 var defer = new Defer(() => called = true);
+                Assert.False(called);
+            }));
+
+            // Garbage collection will put the finalizer in a queue,
+            // so we need to wait for the queue to empty.
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            Assert.False(called);
+        }
+    }
+
+    public class AsyncDeferTests
+    {
+        [Fact]
+        public async Task InvokesCallbackWhenDisposed()
+        {
+            bool called = false;
+            await using (new AsyncDefer(() => { called = true; return Task.CompletedTask; }))
+            {
+                Assert.False(called);
+            }
+
+            Assert.True(called);
+        }
+
+        [Fact]
+        public async Task DisposeIsIdempotent()
+        {
+            bool called = false;
+            Task CallOnlyOnceAsync()
+            {
+                if (called)
+                {
+                    throw new InvalidOperationException("The callback was called more than once!");
+                }
+                else
+                {
+                    called = true;
+                    return Task.CompletedTask;
+                }
+            }
+
+            var defer = new AsyncDefer(CallOnlyOnceAsync);
+            Assert.False(called);
+
+            await defer.DisposeAsync();
+            Assert.True(called);
+
+            await defer.DisposeAsync();
+            Assert.True(called);
+        }
+
+        [Fact]
+        public void DoesNotInvokeCallbackWhenFinalized()
+        {
+            bool called = false;
+
+            // Allocate an AsyncDefer instance in a method scope
+            Func.Invoke(Unit.Close(() =>
+            {
+                var defer = new AsyncDefer(() => { called = true; return Task.CompletedTask; });
                 Assert.False(called);
             }));
 
